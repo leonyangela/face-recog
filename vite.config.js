dc8d3e418e4f72ff7de9e7807174dff2833cfbd5
leonyangela <38250310+leonyangela@users.noreply.github.com>
2026-01-20 09:30:04 +1000

    Working on fixing deployment error

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  optimizeDeps: {
-    include: [
-      '@emotion/react',
-      '@emotion/styled',
-      '@emotion/cache'
-    ],
-  },
+  // optimizeDeps: {
+  //   include: [
+  //     '@emotion/react',
+  //     '@emotion/styled',
+  //     '@emotion/cache'
+  //   ],
+  // },
 });
